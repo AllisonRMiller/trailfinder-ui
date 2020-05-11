56f10328faf984a5a0a914b0847443d316555ab5
AllisonRMiller <60373159+AllisonRMiller@users.noreply.github.com>
2020-05-11 19:37:34 -0400

    Evening commit
    Geocoding successfully returns lat/lng, getting react talking to laravel next

--- trailfinder-ui/src/landing.js
+++ trailfinder-ui/src/landing.js
@@ -1,45 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     Link
-  } from 'react-router-dom';
-import { Row, Col, Jumbotron, Button, Container, FormGroup, Form, Label, Input } from 'reactstrap';
+} from 'react-router-dom';
+import PlacesAutocomplete, {
+    geocodeByAddress,
+    geocodeByPlaceId,
+    getLatLng,
+} from 'react-places-autocomplete';
+import { Row, Col, Jumbotron, Button, Container, FormGroup, Form, Label, Input, Dropdown } from 'reactstrap';
 import logo from './TrailfinderLogo2.png';
 import './landing.css';
 
 
+function Landing() {
+
+    const [address, setAddress] = useState('');
+    const handleChange = (e) => {
+        console.log(e);
+        setAddress(e)
+    }
 
 
-const Landing = (props) => {
+    const handleSelect = () => 
+    geocodeByAddress(address)
+        .then(results => getLatLng(results[0]))
+        .then(latLng => axios.post(`https://jsonplaceholder.typicode.com/users`,{ latLng })
+        )
+        .catch(error => console.error('Error', error));
+
+
+
     return (
         <div>
             <Row>
                 <Col>
                     <Jumbotron id="landing-jumbo" className="jumbotron-fluid text-center m-0 justify-content-center bg-dark text-light">
                         <Container fluid>
-                        <img src={logo}></img>
+                            <img src={logo}></img>
                             {/* <h1 className="display-3">Trailfinder</h1> */}
                             <p className="lead mt-3">Find trails, plan trips. Easy.</p>
                             {/* <hr className="my-2" /> */}
-                        <Row className="mt-5">
-                            <Col className="d-flex justify-content-center">
-                            <Form className="w-25">
-                                    <FormGroup>
-                                        <Label for="Search">Where are you going?</Label>
-                                        <Input //className="w-25"
+                            <Row className="mt-5">
+                                <Col className="d-flex justify-content-center">
+                                    <Form className="w-25">
+                                        <FormGroup>
+                                            <Label>Where are you going?</Label>
+                                            <PlacesAutocomplete
+                                                value={address}
+                                                onChange={handleChange}
+                                                // onSubmit={handleSelect}
+                                            >
+                                                {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                                                    <div>
+                                                        <Input
+                                                            {...getInputProps({
+                                                                placeholder: 'Search Places ...',
+                                                                className: 'location-search-input',
+                                                            })}
+                                                        />
+                                                        <Dropdown>
+                                                            {loading && <div>Loading...</div>}
+                                                            {suggestions.map(suggestion => {
+                                                                const className = suggestion.active
+                                                                    ? 'suggestion-item--active bg-info text-dark'
+                                                                    : 'suggestion-item bg-light text-dark';
+                                                                return (
+                                                                    <div
+                                                                        {...getSuggestionItemProps(suggestion, {
+                                                                            className,
+
+                                                                        })}
+                                                                    >
+                                                                        <span>{suggestion.description}</span>
+                                                                    </div>
+                                                                );
+                                                            })}
+                                                        </Dropdown>
+                                                    </div>
+                                                )}
+                                            </PlacesAutocomplete>
+                                            {/* <Input //className="w-25"
                                             type="search"
                                             name="search"
                                             id="exampleSearch"
                                             placeholder="search placeholder"
-                                        />
-                                    </FormGroup>
-                                    <Button color="primary">Take a hike!</Button>
-                                </Form>
-                                
-                            </Col>
-                        </Row>
+                                        /> */}
+                                        </FormGroup>
+                                        <Button onClick={handleSelect} color="primary">Take a hike!</Button>
+                                        {/* Post user input to path */}
+                                    </Form>
+
+                                </Col>
+                            </Row>
                         </Container>
                     </Jumbotron>
                 </Col>
--- trailfinder-ui/src/search.js
+++ trailfinder-ui/src/search.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import axios from 'axios';
+import PlacesAutocomplete from 'react-places-autocomplete';
+import {
+    geocodeByAddress,
+    geocodeByPlaceId,
+    getLatLng,
+  } from 'react-places-autocomplete';
+
+// Get user input from path
+// Parse input for geocode call
+
+// geocoding call needs to go here . AIzaSyCLOfSGqZe0xwJF7ytbjZs9qrJlxDPg2Bk
 
 
-async function phoneHome(slat,slong) {
+
+// Call to Hiking Project API
+async function phoneHome(slat, slong) {
     var link = "https://www.hikingproject.com/data/get-trails?";
-    var res = "";
+    // var res = "";
     await axios.get(link, {
         params: {
             lat: slat,
-        lon: slong
-        maxDistance: "30";
-        key: "200747499-3d3cab16ebe912a88d76d82693eee11c",
-}
-}
-)
+            lon: slong,
+            maxDistance: "30",
+            key: "200747499-3d3cab16ebe912a88d76d82693eee11c",
+        }
+    }
+    )
         .then(function (response) {
             console.log(response)
         })
